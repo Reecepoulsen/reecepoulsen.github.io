69408157a4258f82f1db70f96b9a4a708c9855d0
Reecepoulsen <dreecepoulsen@gmail.com>
2021-04-24 18:52:36 -0600

    added js and css for last updated

--- js/getCurDate.js
+++ js/getCurDate.js
@@ -1,2 +1,4 @@
 let date = new Date();
 document.getElementById("curYear").textContent = date.getFullYear();
+let lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = lastModified;
