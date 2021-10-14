9ca4681bdecbe2e10dcf91399521cc8b40fb04f2
Reecepoulsen <dreecepoulsen@gmail.com>
2021-10-13 21:39:18 -0600

    added link on main page to weeks 4 and 5

--- frontend2/js/main.js
+++ frontend2/js/main.js
@@ -12,6 +12,14 @@ const links = [
     {
         label: "Week 3",
         url: "./week3/index.html"
+    },
+    {
+        label: "Week 4",
+        url: "./week4/index.html"
+    },
+    {
+        label: "Week 5",
+        url: "./week5/index.html"
     }
 ];
 
