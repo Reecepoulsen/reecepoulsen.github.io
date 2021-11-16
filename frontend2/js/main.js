ae8bdd97bcc6f6e80af21bd152f5f1b1e79d8176
Reecepoulsen <dreecepoulsen@gmail.com>
2021-11-15 21:45:21 -0700

    added links and week9 team activity

--- frontend2/js/main.js
+++ frontend2/js/main.js
@@ -32,6 +32,14 @@ const links = [
     {
         label: "Week 8",
         url: "./week8/index.html"
+    },
+    {
+        label: "Week 9",
+        url: "./week9/index.html"
+    }, 
+    {
+        label: "Week 10",
+        url: "./week10/index.html"
     }
 ];
 
